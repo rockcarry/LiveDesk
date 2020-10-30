67bfc51408ffd2a2e3768839ce89e2b2cff56bfd
chenk <chenk@apical.com.cn>
2020-10-30 12:19:07 +0800

    更新 librtmp 库，实现 rtmp 服务器连接状态检测，并自动开始或停止推流

--- RtmpPusher/rtmppusher.c
+++ RtmpPusher/rtmppusher.c
@@ -19,37 +19,60 @@ typedef struct {
     CODEC    *aenc;
     CODEC    *venc;
 
-    #define TS_EXIT  (1 << 0)
-    #define TS_START (1 << 1)
+    #define TS_EXIT           (1 << 0)
+    #define TS_START          (1 << 1)
+    #define TS_UPSTREAM_START (1 << 2)
     uint32_t  status;
     pthread_t pthread;
     uint8_t   buffer[2 * 1024 * 1024];
 } RTMPPUSHER;
 
+static void rtmppush_start_upstream(RTMPPUSHER *pusher, int start)
+{
+    if (start) {
+        if ((pusher->status & TS_UPSTREAM_START) == 0) {
+            pusher->status |= TS_UPSTREAM_START;
+            codec_reset(pusher->aenc, CODEC_RESET_CLEAR_INBUF|CODEC_RESET_CLEAR_OUTBUF|CODEC_RESET_REQUEST_IDR);
+            codec_reset(pusher->venc, CODEC_RESET_CLEAR_INBUF|CODEC_RESET_CLEAR_OUTBUF|CODEC_RESET_REQUEST_IDR);
+            codec_start(pusher->aenc, 1);
+            codec_start(pusher->venc, 1);
+            adev_start (pusher->adev, 1);
+            vdev_start (pusher->vdev, 1);
+        }
+    } else {
+        if ((pusher->status & TS_UPSTREAM_START) != 0) {
+            pusher->status |= TS_UPSTREAM_START;
+            codec_start(pusher->aenc, 0);
+            codec_start(pusher->venc, 0);
+            adev_start (pusher->adev, 0);
+            vdev_start (pusher->vdev, 0);
+        }
+    }
+}
+
 static void* rtmppush_thread_proc(void *argv)
 {
     RTMPPUSHER *pusher = (RTMPPUSHER*)argv;
-    int aenctype = strcmp(pusher->aenc->name, "aacenc") == 0, framesize, readsize;
+    int aenctype = strcmp(pusher->aenc->name, "aacenc") == 0, framesize, readsize, connected;
 
     while (!(pusher->status & TS_EXIT)) {
         if (!(pusher->status & TS_START)) {
-            usleep(100*1000);
-            continue;
+            rtmppush_start_upstream(pusher, 0);
+            usleep(100*1000); continue;
         }
+
+        connected = rtmp_push_conn(pusher->rtmp) == 0;
+        rtmppush_start_upstream(pusher, connected);
+        if (!connected) { usleep(100*1000); continue; }
 
         readsize = codec_read(pusher->aenc, pusher->buffer, sizeof(pusher->buffer), &framesize, 16);
         if (readsize > 0) {
-            if (aenctype) {
-                rtmp_push_aac (pusher->rtmp, pusher->buffer, readsize);
-            } else {
-                rtmp_push_alaw(pusher->rtmp, pusher->buffer, readsize);
-            }
+            if (aenctype) rtmp_push_aac (pusher->rtmp, pusher->buffer, readsize);
+            else          rtmp_push_alaw(pusher->rtmp, pusher->buffer, readsize);
         }
 
         readsize = codec_read(pusher->venc, pusher->buffer, sizeof(pusher->buffer), &framesize, 16);
-        if (readsize > 0) {
-            rtmp_push_h264(pusher->rtmp, pusher->buffer, readsize);
-        }
+        if (readsize > 0) rtmp_push_h264(pusher->rtmp, pusher->buffer, readsize);
     }
     return NULL;
 }
@@ -97,19 +120,6 @@ void rtmppusher_start(void *ctxt, int start)
 {
     RTMPPUSHER *pusher = (RTMPPUSHER*)ctxt;
     if (!pusher) return;
-    if (start) {
-        pusher->status |= TS_START;
-        codec_reset(pusher->aenc, CODEC_RESET_CLEAR_INBUF|CODEC_RESET_CLEAR_OUTBUF|CODEC_RESET_REQUEST_IDR);
-        codec_reset(pusher->venc, CODEC_RESET_CLEAR_INBUF|CODEC_RESET_CLEAR_OUTBUF|CODEC_RESET_REQUEST_IDR);
-        codec_start(pusher->aenc, 1);
-        codec_start(pusher->venc, 1);
-        adev_start (pusher->adev, 1);
-        vdev_start (pusher->vdev, 1);
-    } else {
-        pusher->status &=~TS_START;
-        codec_start(pusher->aenc, 0);
-        codec_start(pusher->venc, 0);
-        adev_start (pusher->adev, 0);
-        vdev_start (pusher->vdev, 0);
-    }
+    if (start) pusher->status |= TS_START;
+    else       pusher->status &=~TS_START;
 }
--- librtmp/librtmp.h
+++ librtmp/librtmp.h
@@ -5,6 +5,7 @@
 
 void* rtmp_push_init(char *url, uint8_t *aac_dec_spec);
 void  rtmp_push_exit(void *ctxt);
+int   rtmp_push_conn(void *ctxt);
 void  rtmp_push_h264(void *ctxt, uint8_t *data, int len);
 void  rtmp_push_alaw(void *ctxt, uint8_t *data, int len);
 void  rtmp_push_aac (void *ctxt, uint8_t *data, int len);
