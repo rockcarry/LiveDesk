4b5d8a27a4befcdbc78a112da3fd9ac9ceecbb2c
chenk <chenk@apical.com.cn>
2021-04-02 17:29:52 +0800

    优化 ffrdp 的按键处理的代码

--- ffrdp/keybd.c
+++ ffrdp/keybd.c
@@ -6,5 +6,7 @@ void  ffkeybd_exit(void *ctx) {}
 
 void ffkeybd_event(void *ctx, uint8_t key, uint8_t scancode, uint8_t flags1, uint8_t flags2)
 {
-    keybd_event(key, scancode, (flags1 & (1 << 7)) ? 0 : KEYEVENTF_KEYUP, 0);
+    DWORD dwFlags = ((flags1 & (1 << 0)) ? KEYEVENTF_EXTENDEDKEY : 0)
+                  | ((flags1 & (1 << 7)) ? KEYEVENTF_KEYUP       : 0);
+    keybd_event(key, MapVirtualKey(key, 0), dwFlags, 0);
 }
